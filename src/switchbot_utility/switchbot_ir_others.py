c2d301629c3bd680c674a01f5dbb5549634b8183
iCarrot0605 <icarrot@mac.com>
2022-11-15 22:38:46 +0900

    Added IrOthers class

--- src/switchbot_utility/switchbot_ir_others.py
+++ src/switchbot_utility/switchbot_ir_others.py
@@ -3,22 +3,19 @@ from .switchbot_device import SwitchbotDevice
 
 class IrOthers(SwitchbotDevice):
     """IR viertual device others class"""
-    
+
     def __init__(self, deviceId):
         super().__init__(deviceId)
-        
+
     def customize(self, button_name: str):
         """Execute customized command
-        
+
         button_name should be quoted.
         e.g. "on", "off"
         """
-        
-        body = {
-            "commandType": "customize",
-            "parameter": "default"
-        }
-        body["command"] = str(button_name)
-        
+
+        body = {"commandType": "customize", "parameter": "default"}
+        body["command"] = button_name
+
         response = self.command(self.deviceId, body)
         return response.text
